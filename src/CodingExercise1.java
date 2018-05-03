d64012389412c2d1dc47ba45ea6f79bb48c7ca5b
Isaacpants <league@WTS-IM-6.attlocal.net>
2018-05-02 18:26:48 -0700

    written test fin code exam 1 and 2 fin

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -5,15 +5,30 @@ import org.jointheleague.graphical.robot.Robot;
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
 
-    public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
+	public static void main(String[] args) {
+		Robot r2d2 = new Robot();
+		r2d2.hide();
+		// 3. ask the user what color they would like the Robot to draw
+		String c = JOptionPane.showInputDialog("Wut color black or blue");
+		// 4. use an if/else statement to set the pen color that the user requested
+		// (minimum of 2 colors)
+		if (c.equals("blue")) {
+			r2d2.setPenColor(0, 17, 255);
 
-   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+		} else {
 
-   	 // 2. set the pen width to 10
+		}
+		// 2. set the pen width to 10
+		r2d2.penDown();
+		r2d2.setPenWidth(10);
+		r2d2.setSpeed(1000);
 
-   	 // 1. make the Robot draw a shape
-   	 
-    }
+		// 1. make the Robot draw a shape
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(90);
+			r2d2.turn(90);
+		}
+
+	}
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,17 +1,21 @@
 
-
 /* Level 0 Exam: Coding  Exercise #2 */
 import javax.swing.JOptionPane;
 
 /**
- * Write a program that asks the user for their age. 
- * Tell them which year they were born. 
- * If they are over 30, tell them they are too old to play this game.
+ * Write a program that asks the user for their age. Tell them which year they
+ * were born. If they are over 30, tell them they are too old to play this game.
  **/
 
 public class CodingExercise2 {
+	public static void main(String[] args) {
+		String ag = JOptionPane.showInputDialog("How old r u");
+		int a = Integer.parseInt(ag);
+	JOptionPane.showMessageDialog(null, 2018-a);
+		if (a > 30) {
+			JOptionPane.showMessageDialog(null, "U 2 OLD SON");
 
+		}
+
+	}
 }
-
-
-
